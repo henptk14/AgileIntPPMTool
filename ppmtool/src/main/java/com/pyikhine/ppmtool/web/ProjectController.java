369b798ec410cf576a06bc375f375635fa2c248e
Pyi Thein Khine <37124353+henptk14@users.noreply.github.com>
2019-08-08 15:35:50 -0700

    Implemented PUT request

--- ppmtool/src/main/java/com/pyikhine/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/pyikhine/ppmtool/web/ProjectController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,5 +61,12 @@ public class ProjectController {
 		projectService.deleteProjectByIdentifier(projectId);
 		
 		return new ResponseEntity<String>("Project with ID: '" + projectId + "' is deleted", HttpStatus.OK);
+	}
+	
+	@PutMapping("")
+	public ResponseEntity<?> updateProject(@RequestBody Project project) {
+		projectService.saveOrUpdateProject(project);
+		
+		return new ResponseEntity<Project>(project, HttpStatus.OK);
 	}
 }
