70cd8178852cbce5fe1d6add1fe7abc009e690cd
Pyi Thein Khine <37124353+henptk14@users.noreply.github.com>
2019-08-08 14:54:16 -0700

    Implemented GET request using project_identifier

--- ppmtool/src/main/java/com/pyikhine/ppmtool/repository/ProjectRepository.java
+++ ppmtool/src/main/java/com/pyikhine/ppmtool/repository/ProjectRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.pyikhine.ppmtool.domain.Project;
 
 public interface ProjectRepository extends CrudRepository<Project, Long>{
-
+	Project findByProjectIdentifier(String projectId);
 }
